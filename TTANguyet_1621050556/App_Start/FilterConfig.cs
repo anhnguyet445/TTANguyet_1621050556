b7ad82723c3c326341e04c8d0089677df09a36cb
TA THI ANH NGUYET <1621050556@student.humg.edu.vn>
2020-12-24 11:35:41 +0700

    Add project files.

---
+++ TTANguyet_1621050556/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TTANguyet_1621050556
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
